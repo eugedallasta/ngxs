fa9978b8d08ff11ab6432e1c8d48d66afb9d7a39
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-09-29 14:08:24 -0300

    install ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { AppComponent } from './app.component';
 import { ListarPostComponent } from './components/listar-post/listar-post.component';
 import { NuevoPostComponent } from './components/nuevo-post/nuevo-post.component';
+import { environment } from 'src/environments/environment';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,10 @@ import { NuevoPostComponent } from './components/nuevo-post/nuevo-post.component
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    NgxsModule.forRoot([], { developmentMode: !environment.production }),
+    NgxsReduxDevtoolsPluginModule.forRoot({ disabled: environment.production }),
+    NgxsLoggerPluginModule.forRoot({ disabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/listar-post/listar-post.component.ts
+++ src/app/components/listar-post/listar-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/interface/post';
 
 @Component({
   selector: 'app-listar-post',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-post.component.css']
 })
 export class ListarPostComponent implements OnInit {
+  listPosts: Post[] = [
+    {
+      id: 'grfrfr-fr',
+      nombre: 'Post 1',
+      descripcion: 'Contenido del post 1',
+    },
+    {
+      id: '2',
+      nombre: 'Post 2',
+      descripcion: 'Contenido del post 2'
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  eliminarPost(id: string) {
+    this.listPosts = this.listPosts.filter(post => post.id !== id);
   }
 
 }
